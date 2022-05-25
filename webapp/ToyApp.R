11a7179fc3800d519173ca472277e840c17b7b6d
kevfo <75442525+kevfo@users.noreply.github.com>
2022-05-26 00:18:01 +0800

    Finished Toy Application

--- webapp/ToyApp.R
+++ webapp/ToyApp.R
@@ -1,8 +1,8 @@
 library(shiny) 
 
 # Be sure to change the working directory if need be:
-setwd("C:/Users/Kevin/Desktop/Projects/R/TeamAthena2022/analysis/")
-source('model.R')
+#setwd("C:/Users/Kevin/Desktop/Projects/R/TeamAthena2022/analysis/")
+#source('model.R')
 
 choices <- c('yes', 'no')
 
@@ -11,24 +11,32 @@ ui <- fluidPage(
     tabPanel("Quick Check", br(),
              sidebarLayout(
                sidebarPanel(
-                 numericInput('sc', 'Serm Creatinine Level (mgs / dl)', 0, min = 0),
-                 numericInput('sg', 'Specific Gravity', 0, min = 0),
-                 numericInput('al', 'Albumin Level', 0, min = 0),
-                 selectInput('htn', 'Has Hypertension?', choices),
-                 numericInput('sod', 'Sodium Level (mEg / L)', 0, min = 0),
-                 numericInput('bu', 'Blood Urea Level (mgs / dl)', 0, min = 0),
-                 actionButton('submit', 'Check Status')
+                 numericInput('sc', 'Serum Creatinine (sc) Level (mgs / dl)', 0, min = 0),
+                 numericInput('sg', 'Specific Gravity (sg)', 0, min = 0),
+                 numericInput('al', 'Albumin Level (al)', 0, min = 0),
+                 selectInput('htn', 'Has Hypertension (htn)?', choices),
+                 numericInput('sod', 'Sodium (sod) Level (mEg / L)', 0, min = 0),
+                 numericInput('bu', 'Blood Urea (bu) Level (mgs / dl)', 0, min = 0),
+                 actionButton('submit', 'Predict Status')
                ),
                mainPanel(
+                 h3("Data Entered"),
+                 tableOutput('dataE1'),
                  h3("Concensus"),
-                 textOutput('status')
+                 htmlOutput('status')
                )
              )),
-    tabPanel("Multiple Data", br())
+    tabPanel("Multiple Data", br(),
+             fileInput('fileUpload', "Upload a .csv file!", multiple = T, accept = '.csv'),
+             actionButton('mdSubmit', "Predict Status"), 
+             br(), br(), br(), 
+             dataTableOutput('testme')
+    )
   )
 )
 
 server <- function(input, output, session) {
+  
   # == Helper function to make a data frame ==
   makeDF <- reactive({
     if (input$submit > 0) {
@@ -47,10 +55,46 @@ server <- function(input, output, session) {
     dataToReturn$df <- makeDF()$df
   })
   
-  output$status <- renderText({
-    if (is.null(dataToReturn$df)) {return()}
-    predict(rfWithVip, dataToReturn$df) %>% as.character()
+  output$dataE1 <- renderTable({
+    if (is.null(dataToReturn$df)) {return(data.frame(sc = NA, sg = NA, 
+                                                     al = NA, htn = NA,
+                                                     sod = NA, bu = NA))}
+    dataToReturn$df
   })
+  
+  output$status <- renderText({
+    if (is.null(dataToReturn$df)) {return("Enter some data to get started!")}
+    pStatus <- predict(rfWithVip, dataToReturn$df) %>% as.character()
+    if (pStatus == 'ckd') {
+      return("<b><font color = \"red\">Patient has kidney disease</font></b>")
+    } else {
+      return("<b><font color = \"green\">Patient does not have kidney disease</font></b>")
+    }
+  })
+  
+  # == .csv file input == 
+  
+  patientData <- reactive({
+    req(input$fileUpload)
+    
+    pData <- tools::file_ext(input$fileUpload$name)
+    switch(pData, 
+           csv = vroom::vroom(input$fileUpload$datapath, delim = ','),
+           validate("Please only upload .csv files!"))
+  })
+  
+  dataToCompute <- reactiveValues(data = NULL)
+  observeEvent(input$mdSubmit, {
+    dataToCompute$df <- patientData()
+    dataToCompute$allPred <- predict(rfWithVip, dataToCompute$df)
+  })
+  
+  output$testme <- renderDataTable({
+    if (is.null(dataToCompute$df)) {return(data.frame(`Subject Number` = NA, Predictions = NA))}
+    data.frame(`Subject Number` = 1:length(dataToCompute$allPred), 
+                    Predictions = ifelse(dataToCompute$allPred == 'ckd', "Has Kidney Disease",
+                                         "No Kidney Disease"))
+  }, options = list(pagelength = 5))
 }
 
 shinyApp(ui, server)
